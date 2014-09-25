9c291909d6c4df54a714609b196057ee08a98a14
Joan Torres <joan.torres@upr.edu>
2014-09-25 16:31:53 -0700

    ADD SOME MODEL STRUCTURE

--- public/js/application.js
+++ public/js/application.js
@@ -2,3 +2,4 @@ var GameController = function(){
 
 
 }
+// if ("0011101".match(/111/)){ console.log(true) };
--- public/js/board.js
+++ public/js/board.js
@@ -1,4 +1,28 @@
 var Board = function(){
 
+  this.boardState = [0,0,0,0,0,0,0,0,
+                       0,0,0,0,0,0,0,
+                       0,0,0,0,0,0,0,
+                       0,0,0,0,0,0,0,
+                       0,0,0,0,0,0,0,
+                       0,0,0,0,0,0,0,
+                  ];
+}
+
+Board.prototype = {
+
+  getRow: function(){
+
+  },
+  getColumn: function(){
+
+  },
+  getDiagonal: function(){
+
+  },
+
+  updateBoard: function(){
+
+  }
 
 }
