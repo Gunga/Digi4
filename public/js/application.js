2d1932216a9aa9c2fe1b88de715c165da8d732cb
Joan Torres <joan.torres@upr.edu>
2014-09-25 17:33:06 -0700

    ADD LOGIC DESCRIPTION

--- public/js/application.js
+++ public/js/application.js
@@ -1,4 +1,41 @@
+// resetGame  takes button click as input
+//            calls resetGamestate (Board.boardstate = [0...])
+//            call updateDisplay
+//            output boardState is set to all zeroes
+
+// turnIndicator is var that is boardState [0]
+
+// win?       input is none -- but is called by Gamecontroller objectd
+//            calls get rol/col/diag for each appropriate num
+//            searches for four in a row
+//            if win, calls:
+               // winnerAlert
+               // resetGame
+
+// game logic input is onClick
+                // placeToken -- modifies boardState
+                // updateDisplay -- sends view instructions
+                // updateTurnIndicator -- sends view instructions
+                // win?
+                // changeTurn
+
+//changeTurn  input none, called by controller (logic)
+                // changes boardState[0] from 0 to 1 or 1 to 0
+
+
+//placeToken    input is button click value
+//              will pass range 1..7 to updateBoard from buttonClick
+
+//updateBoard
+//        input takes arg of range 1..7
+//        change value of boardState index to 1 or 2, which is next 'open position'
+
+//turn?         // almost deprecated
+                // checks value of boardState[0] -- only useful for AI
+
 var GameController = function(){
+
+
 
 
 }
--- public/js/view.js
+++ public/js/view.js
@@ -1,3 +1,10 @@
+// updateDisplay    input is boardState, which is array of 43 numbers, called by Controller
+                    // NOTE!!!!!!!!!!!!! IGNORE THE FIRST ITEM IN ARRAY, WHICH IS TURNINDICATOR
+//                  output is color settings for each <li>
+
+//updateTurnIndicator   uses boardState to update display Turn based on boardState[0]
+
+
 var View = function(){
 
 
