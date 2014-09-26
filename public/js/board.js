582ff0894ac6819e1ffea3ccfd9d22d9a9a73e48
Jess B Heron <jessbheron@gmail.com>
2014-09-26 12:30:59 -0700

    Functional updateBoard function

--- public/js/board.js
+++ public/js/board.js
@@ -13,6 +13,15 @@
 var Board = function(){
   this.boardState =
     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+// ===================================================================
+//    TEST ARRAY 'FOO' WILL CREATE 43 ELEMENT ARRAY WITH INTEGERS 0-42
+//   var foo = [];
+//   for (var i = 0; i <= 42; i++) {
+//      foo.push(i);
+//   }
+//   this.boardState = foo
+//                 REMOVE TEST ARRAY FOR PRODUCTION !!!
+// ===================================================================
 };
 
 Board.prototype = {
@@ -21,25 +30,48 @@ Board.prototype = {
     var row = [];
     for (var i = 0; i < this.boardState.length; i++) {
       if ((rowNum - 1) == Math.floor( (i - 1)/7 ) )
-        row.push(i);
+        row.push(this.boardState[i]);
     }
-    console.log(row)
-    return row
+    return row.join("")
   },
   getColumn: function(colNum){
     var col = [];
     for (var i = 0; i < this.boardState.length; i++) {
-      if (colNum  == (i - 1)%7 )
-        col.push(i);
+      if (colNum - 1 == (i - 1)%7 )
+        col.push(this.boardState[i]);
     }
-    console.log(col)
-    return col
+    return col.join("")
   },
   getDiagonal: function(){
 
   },
 
-  updateBoard: function(){
+  // returns indicies of column
+  updateBoardHelper: function(colNum){
+    var col = [];
+    for (var i = 0; i < this.boardState.length; i++) {
+      if (colNum - 1 == (i - 1)%7 )
+        col.push(i);
+    }
+    return col
+  },
+
+  // GOTCHA! Check Boardstate dependency of boardState[0] on changing i to 1 or 2
+  updateBoard: function(colNum){
+    var columnPositions = this.updateBoardHelper(colNum); // array [i, i...]
+
+
+
+
+    var columnValues = this.getColumn(colNum);            // string "val1val2"
+    var emptyCell = columnValues.lastIndexOf("0");   //will return 0-5
+
+    if (this.boardState[0] == 0)
+      this.boardState[columnPositions[emptyCell]] = 1;
+    if (this.boardState[0] == 1)
+      this.boardState[columnPositions[emptyCell]] = 2;
+
+    return columnPositions[emptyCell]
 
   },
   reset: function(){
