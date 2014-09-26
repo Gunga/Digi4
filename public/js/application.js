a29af6449f20064835ac9212ec1cb509233159f4
Joan Torres <joan.torres@upr.edu>
2014-09-26 04:06:08 -0400

    ADD BASIC GAME CONTROLLER LOGIC
    I added some of the logic behind the controller. There are some dependencies due to the fact that the board and view aren't finished yet. However, the click on the column should trigger the appropriate actions. Beyond the controller itself, the buttons should have an id in order to make this simpler and the layout should load the controller AFTER the board.js and view.js have loaded. Otherwise, their prototypes wouldn't be available.

--- public/js/application.js
+++ public/js/application.js
@@ -33,10 +33,96 @@
 //turn?         // almost deprecated
                 // checks value of boardState[0] -- only useful for AI
 
-var GameController = function(){
-
-
-
+var GameController = function(board, view){
+    var that = this;
+    this.board = board;
+    this.view = view;
+    this.win = false;
+    this.turnIndicator = board.boardState[0];
+     $('form').on('click', this.gameLogic.bind(that));
 
 }
-// if ("0011101".match(/111/)){ console.log(true) };
+
+GameController.prototype = {
+
+    resetGame: function(){
+        this.board = new Board();
+    },
+
+    checkForWin: function(){
+
+        this.checkRowsForWin();
+        this.checkColumnsForWin();
+        this.checkDiagonalsForWin();
+
+        return this.win;
+    },
+
+    checkRowsForWin: function(){
+        for (var i = 1; i <= 6; i++) {
+            var row = this.board.getRow(i);
+            if (row.match(/[1]{4}?[0]{4}?/))
+                this.win = true;
+        };
+    },
+
+    checkColumnsForWin: function(){
+        for (var i = 1; i <= 7; i++) {
+            var column = this.board.getColumn(i);
+            if (column.match(/[1]{4}?[0]{4}?/))
+                this.win = true;
+        };
+    },
+
+    checkDiagonalsForWin: function(){
+        for (var i = 1; i <= 12; i++) {
+            var diagonal = this.board.getDiagonal(i,"backslash");
+            if (diagonal.match(/[1]{4}?[0]{4}?/))
+                this.win = true;
+        };
+
+        for (var i = 1; i <= 12; i++) {
+            var diagonal = this.board.getDiagonal(i, "slash");
+            if (diagonal.match(/[1]{4}?[0]{4}?/))
+                this.win = true;
+        };
+    },
+
+    changeTurn: function(){
+        this.turnIndicator ^= 1;
+        this.board.boardState[0] = this.turnIndicator;
+        console.log("Current Turn: ", this.turnIndicator);
+        this.view.updateTurnIndicator();
+    },
+
+    placeToken: function(column){
+        console.log("Column: ", column);
+        this.updateBoard(column)
+    },
+
+    updateBoard: function(column){
+        index = this.board.updateBoard(column);
+
+        this.view.updateDisplay(index);
+    },
+
+    turn: function(){
+        this.turnIndicator
+    },
+
+    gameLogic: function(e){
+        e.preventDefault();
+
+        this.placeToken(e.target.id);       
+
+        if (this.checkForWin()){ 
+            this.view.winnerAlert();
+        }else{
+            this.changeTurn();
+        }
+
+    }
+
+}
+
+gc = new GameController(new Board(),new View());
