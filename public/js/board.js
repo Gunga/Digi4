d15ff6d98d5b3c018ca7e95bcca2da503db182ba
Jess B Heron <jessbheron@gmail.com>
2014-09-25 23:29:03 -0700

    update model

--- public/js/board.js
+++ public/js/board.js
@@ -1,4 +1,4 @@
-//getRow  input takes argument of range 1..6
+//getRow  input takes argument of range 1..6, which is row #
 //        output array of 7 integers either 0, 1(Red), 2(Black)
 //getCol  input takes arg of range 1..7
 //        output array of 7 integers either 0, 1(Red), 2(Black)
@@ -11,23 +11,29 @@
 
 
 var Board = function(){
-
-  this.boardState = [0,0,0,0,0,0,0,0,
-                       0,0,0,0,0,0,0,
-                       0,0,0,0,0,0,0,
-                       0,0,0,0,0,0,0,
-                       0,0,0,0,0,0,0,
-                       0,0,0,0,0,0,0,
-                  ];
-}
+  this.boardState =
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+};
 
 Board.prototype = {
 
-  getRow: function(){
-
+  getRow: function(rowNum){
+    var row = [];
+    for (var i = 0; i < this.boardState.length; i++) {
+      if ((rowNum - 1) == Math.floor( (i - 1)/7 ) )
+        row.push(i);
+    }
+    console.log(row)
+    return row
   },
-  getColumn: function(){
-
+  getColumn: function(colNum){
+    var col = [];
+    for (var i = 0; i < this.boardState.length; i++) {
+      if (colNum  == (i - 1)%7 )
+        col.push(i);
+    }
+    console.log(col)
+    return col
   },
   getDiagonal: function(){
 
@@ -35,7 +41,10 @@ Board.prototype = {
 
   updateBoard: function(){
 
+  },
+  reset: function(){
+    this.boardState =  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
   }
-
-}
+};
+var test = new Board();
 
